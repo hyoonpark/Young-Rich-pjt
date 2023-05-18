23e40ace83656cfd981591d3201b565e09857e61
hyoonpark <the.day0619@gmail.com>
2023-05-18 11:29:37 +0900

    5월 18일 오전 게시판

--- Skeleton/front-server/src/views/ArticleView.vue
+++ Skeleton/front-server/src/views/ArticleView.vue
@@ -16,27 +16,27 @@ export default {
     Article,
   },
   computed:{
-    // isLogin() {
-    //   return this.$store.getters.isLogin // 로그인 여부
-    // }
+    isLogin() {
+      return this.$store.getters.isLogin // 로그인 여부
+    }
   },
   created() {
     this.getArticles()
   },
   methods: {
-    // getArticles() {
-    //   if (this.isLogin) {
-    //     this.$store.dispatch('getArticles')
-    //   } else {
-    //     alert('로그인이 필요한 페이지입니다...')
-    //     this.$router.push({ name: 'LogInView' })
-    //   }
+    getArticles() {
+      if (this.isLogin) {
+        this.$store.dispatch('getArticles')
+      } else {
+        alert('로그인이 필요한 페이지입니다...')
+        this.$router.push({ name: 'LogInView' })
+      }
 
 
-    //   // 로그인이 되어 있으면 getArticles action 실행
-    //   // 로그인 X라면 login 페이지로 이동
+      // 로그인이 되어 있으면 getArticles action 실행
+      // 로그인 X라면 login 페이지로 이동
 
-    // }
+    }
   }
 }
 </script>
