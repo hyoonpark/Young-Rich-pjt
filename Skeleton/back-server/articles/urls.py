417ec751d8671bfaeb631d1a32a55389e1f73a19
hyoonpark <the.day0619@gmail.com>
2023-05-19 16:41:51 +0900

    5월 19일 오후 게시판 백엔드 완료

--- Skeleton/back-server/articles/urls.py
+++ Skeleton/back-server/articles/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('comments/', views.comment_list),
     path('comments/<int:comment_pk>/', views.comment_detail),
     path('articles/<int:article_pk>/comments/', views.comment_create),
+    path('replies/', views.reply_list),
+    path('replies/<int:reply_pk>/', views.reply_detail),
+    path('like/toggle/', views.toggle_like),
+    path('like/status/<str:obj_type>/<int:obj_id>/', views.get_like_status),
 ]
