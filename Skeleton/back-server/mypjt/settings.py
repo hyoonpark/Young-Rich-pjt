397381b32e6af4eebf0fc7fbdc61f928513f276b
전대현 <wjseogus523@naver.com>
2023-05-19 09:57:32 +0900

    5/19 -> merge

--- Skeleton/back-server/mypjt/settings.py
+++ Skeleton/back-server/mypjt/settings.py
@@ -49,8 +49,6 @@ INSTALLED_APPS = [
     'kakaomaps',
     'accounts',
     'finlife',
-    'kakaomaps',
-    'calculators',
     'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
